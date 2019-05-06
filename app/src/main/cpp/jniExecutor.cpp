8f3dc79054281b02c4a6354979924fc0cbd0e09e
MusaMahmood <musasmahmood@gmail.com>
2019-05-06 15:17:45 -0400

    Initial commit. Based on strain gauge application.

---
+++ app/src/main/cpp/jniExecutor.cpp
@@ -0,0 +1,22 @@
+//
+// Created by mahmoodms on 4/3/2017.
+//
+
+/*Additional Includes*/
+#include <jni.h>
+#include <android/log.h>
+
+#define  LOG_TAG "jniExecutor-cpp"
+#define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+
+extern "C" {
+JNIEXPORT jint JNICALL
+Java_com_yeolabgt_mahmoodms_ppg_DeviceControlActivity_jmainInitialization(
+        JNIEnv *env, jobject obj, jboolean initialize) {
+    if (!(bool) initialize) {
+        return 0;
+    } else {
+        return -1;
+    }
+}
+}
